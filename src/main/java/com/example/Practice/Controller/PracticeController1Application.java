ea53beb3a932cd5196f687cc578b9d185b04eddd
Aditya Verma <adiadi7898@gmail.com>
2025-04-14 01:37:00 +0530

    Added more CRUD Operations and Learnt JPA functions in interface

--- src/main/java/com/example/Practice/Controller/PracticeController1Application.java
+++ src/main/java/com/example/Practice/Controller/PracticeController1Application.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeController1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeController1Application.class, args);
-			}
+	}
 }
--- src/main/java/com/example/Practice/Controller/Repository/JournalRepository.java
+++ src/main/java/com/example/Practice/Controller/Repository/JournalRepository.java
@@ -2,5 +2,17 @@ package com.example.Practice.Controller.Repository;
 import com.example.Practice.Controller.Entity.Journal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+/*
+Here whatever new method we declare will be creating a query.
+I did a mistake by keeping random name of methods which I need to take care
+as every word in the method means something.
+For understanding more, read more about JPA.
+ */
 public interface JournalRepository extends JpaRepository<Journal, Integer> {
+    List<Journal> findAllByNameInIgnoreCase(List<String> names);
+    List<Journal> findAllByName(String name);
+    List<Journal> findAllById(Integer id);
+
 }
